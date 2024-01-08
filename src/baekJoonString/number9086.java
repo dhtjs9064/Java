2c37e37c6214b46a9627b8014d4ef988b99f4780
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2024-01-08 18:31:54 +0900

    자바 27866 charAt() 응용

---
+++ src/baekJoonString/number9086.java
@@ -0,0 +1,18 @@
+package baekJoonString;
+
+import java.util.Scanner;
+
+public class number9086 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        // 테스트 개수
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            String S = sc.next();
+            sb.append(S.charAt(0)).append(S.charAt(S.length() - 1)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
