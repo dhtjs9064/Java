ffa3c9956ba2d43af918b4ca257b493e7f2f5341
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2024-01-09 15:17:17 +0900

    .

---
+++ src/BaekJoonDeepening1/number25083.java
@@ -0,0 +1,13 @@
+package BaekJoonDeepening1;
+
+public class number25083 {
+    public static void main(String[] args) {
+        String s = "         ,r'\"7\n"
+                + "r`-_   ,'  ,/\n"
+                + " \\. \". L_r'\n"
+                + "   `~\\/\n"
+                + "      |\n"
+                + "      |";
+        System.out.print(s);
+    }
+}
