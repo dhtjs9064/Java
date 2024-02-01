4b54196e4a6e5688a97a1d29710779dbec0d24e7
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2024-02-01 17:12:00 +0900

    자바 시간복잡도 문제 이해가 어렵다..

---
+++ src/BaekJoonTimeComplexity/number24262.java
@@ -0,0 +1,9 @@
+package BaekJoonTimeComplexity;
+
+public class number24262 {
+    public static void main(String[] args) {
+        // MenOfPassion 알고리즘에 n을 넣을 경우 어차피 한번만 반복됨 즉, 시간 복잡도가 항상 O(1)이 걸리게 된다.
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
