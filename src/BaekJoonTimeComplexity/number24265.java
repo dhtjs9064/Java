678261c4f5deb7593dbb272cbd8894b9b79df537
dhtjs4652 <118519803+dhtjs4652@users.noreply.github.com>
2024-02-06 15:56:30 +0900

    자바 시간복잡도 .

--- src/BaekJoonTimeComplexity/number24265.java
+++ src/BaekJoonTimeComplexity/number24265.java
@@ -9,8 +9,8 @@ public class number24265 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         Long n = Long.parseLong(br.readLine());
-
-        System.out.println(n * (n-1) / 2);
+// for문 그대로 i~n까지 더하는게 아니라 i~n까지의 횟수를 세는 것
+        System.out.println(n * (n - 1) / 2);
         System.out.println(2);
     }
 }
